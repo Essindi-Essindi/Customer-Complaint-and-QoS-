7586cf2ec00c3f1aa5f9a1783b0c4de5aed37686
Steph <yvanstephanee@gmail.com>
2026-08-02 13:11:14 +0100

    Initial

---
+++ src/main/java/customer_complaint/customer_complaint/CustomerComplaintApplication.java
@@ -0,0 +1,13 @@
+package customer_complaint.customer_complaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerComplaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerComplaintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/customer_complaint/customer_complaint/CustomerComplaintApplicationTests.java
@@ -0,0 +1,13 @@
+package customer_complaint.customer_complaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerComplaintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
